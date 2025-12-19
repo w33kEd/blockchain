d0b7a67664e7c15334ac231afa151d0af1f7f63c
w33ked <otieno.phidel11@gmail.com>
2025-12-19 15:58:07 +0300

    created block and associated tests

--- dev-test.js
+++ dev-test.js
@@ -1,4 +1,6 @@
 const Block = require('./block');
 
-const block = new Block('foo', 'bar', 'zoo', 'baz');
-console.log(block.toString());
+const fooBlock = Block.mineBlock(Block.genesis(), 'foo');
+console.log(fooBlock.toString());
+const secondBlock = Block.mineBlock(fooBlock, 'first');
+console.log(secondBlock.toString());
