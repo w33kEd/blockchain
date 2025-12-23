f64197ac297e539a65593fc889606ded7dd4ac32
w33ked <otieno.phidel11@gmail.com>
2025-12-23 16:13:25 +0300

    sync chains on connect

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ const MINE_RATE = 1000;
 const INITIAL_DIFFICULTY = 3;
 
 const GENESIS_DATA = {
-    timestamp: Date.now(),
-    lastHash: '-----',
-    hash: 'hash-one',
+    timestamp: 1,
+    lastHash: '819c45c748c520f49cff5978df48f8e132f9156d7b86745771fbc07f63e8d3da',
+    hash: '88098c55fe454634ae639dff468b00b48eea34e5c415a640dd172435faee787a',
     difficulty: INITIAL_DIFFICULTY,
     nonce: 0,
     data: []
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const request = require('request');
 const bodyParser = require('body-parser')
 const Blockchain = require('./blockchain');
 const PubSub = require('./pubsub');
@@ -7,7 +8,9 @@ const app = express();
 const blockchain = new Blockchain();
 const pubsub = new PubSub({ blockchain });
 
-setTimeout(() => pubsub.broadcastChain(), 1000);
+const DEFAULT_PORT = 3000;
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`
+
 
 app.use(bodyParser.json());
 
@@ -25,7 +28,18 @@ app.post('/api/mine', (req, res) => {
     res.redirect('/api/blocks');
 });
 
-const DEFAULT_PORT = 3000;
+const syncChains = () => {
+   request({ url: `${ROOT_NODE_ADDRESS}/api/blocks` }, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+        const rootChain = JSON.parse(body);
+
+        console.log('replace chain on a sync with', rootChain);
+        blockchain.replaceChain(rootChain);
+    }
+
+   }); 
+};
+
 let PEER_PORT;
 
 if(process.env.GENERATE_PEER_PORT === 'true') {
@@ -33,4 +47,10 @@ if(process.env.GENERATE_PEER_PORT === 'true') {
 }
 
 const PORT = PEER_PORT || DEFAULT_PORT;
-app.listen(PORT, '0.0.0.0', () => console.log(`listening at localhost:${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`listening at localhost:${PORT}`);
+
+    if (PORT !== DEFAULT_PORT ){
+        syncChains();
+    }
+});
