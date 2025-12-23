604157f0b9b833a3959dd56f0518c286b4f277c8
w33ked <otieno.phidel11@gmail.com>
2025-12-23 19:51:16 +0300

    add cryptographic signing and verification for messages

--- wallet/index.test.js
+++ wallet/index.test.js
@@ -1,10 +1,13 @@
 const Wallet = require('./index');
+const { verifySignature } = require('../util');
 
-describe('Wallet', () => {
+describe('WALLET', () => {
     let wallet;
+    let wallet2;
 
     beforeEach(() => {
         wallet = new Wallet();
+        wallet2 = new Wallet();
     });
 
     it('has a `balance`', () => {
@@ -13,5 +16,38 @@ describe('Wallet', () => {
     it('has a `publicKey`', () => {
         console.log(wallet.publicKey);
         expect(wallet).toHaveProperty('publicKey');
+    });
+
+    describe('signing data', () => {
+        const data = 'foo-bar';
+
+        it('verifies a signature', () => {
+            expect(
+                verifySignature({
+                    publicKey: wallet.publicKey,
+                    data,
+                    signature: wallet.sign(data)
+                })
+            ).toBe(true);
+        });
+
+        it('does not verify an invalid signature', () => {
+            expect(
+                verifySignature({
+                    publicKey: new Wallet().publicKey,
+                    data,
+                    signature: new Wallet().sign(data)
+                })
+            ).toBe(false);
+        });
+        it('does not verify an invalid signature from other address', () => {
+            expect(
+                verifySignature({
+                    publicKey: wallet2.publicKey,
+                    data,
+                    signature: wallet.sign(data)
+                })
+            ).toBe(false);
+        });
     });
 });
